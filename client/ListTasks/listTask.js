1b56d3fae3c3267407d628c17722c3695204bdd8
Amari Heeralal <amariheeralal03@gmail.com>
2022-07-06 15:28:56 -0400

    Eleventh

--- client/ListTasks/listTask.js
+++ client/ListTasks/listTask.js
@@ -1,5 +1,13 @@
 Template.listTasks.helpers({
     theTasks(){
         return tododb.find();
+    },
+});
+
+Template.listTasks.events({
+    'click .js-erase'() {
+        let that=this;
+        $("listId").val("hide");
+        console.log("adding");
     }
 })
